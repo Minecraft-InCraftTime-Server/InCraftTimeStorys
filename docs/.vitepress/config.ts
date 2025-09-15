63d1f97c50dc165a583fb64707a8ca82b6c3cd1d
MineSunshineone <107391209+MineSunshineone@users.noreply.github.com>
2025-09-16 00:47:58 +0800

    Fix base path in VitePress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -8,7 +8,7 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: '/story/',
+  base: '/storys/',
 
   lang: 'zh-CN',
   title: 'InCraftTime Stories',
